b14cbfc562553d30196e6460d47b75e39a5d341d
Jessica Yip <jessica.yip011396@gmail.com>
2021-01-09 15:04:53 -0500

    commenting out code

---
+++ formbuilder/containers/FormCreatedContainer.js
@@ -0,0 +1,18 @@
+/*import { connect } from "react-redux";
+
+import FormCreated from "../components/FormCreated";
+
+
+function mapStateToProps(state) {
+  return {
+    schema: state.form.schema,
+    uiSchema: state.form.uiSchema,
+    formData: state.form.formData,
+    publicationStatus: state.publicationStatus
+  };
+}
+
+export default connect(
+  mapStateToProps,
+)(FormCreated);
+*/
---
+++ formbuilder/containers/RecordCreatedContainer.js
@@ -0,0 +1,17 @@
+/*import { connect } from "react-redux";
+
+import RecordCreated from "../components/RecordCreated";
+
+
+function mapStateToProps(state) {
+  return {
+    schema: state.form.schema,
+    uiSchema: state.form.uiSchema,
+    formData: state.form.formData,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+)(RecordCreated);
+*/
